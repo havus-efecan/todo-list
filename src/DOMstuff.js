5eb0cd5cc9608b9076a57279b8c0ba584ee9514d
Efecan Havus <Ehavus@gmail.com>
2023-06-21 16:45:53 -0400

    can create and name new projects

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -1,8 +1,11 @@
+import { addProjectEventListeners } from "./index"
+
 const mainContainer = document.querySelector('.main-container')
 const header = document.querySelector('.header')
 const sidebar = document.querySelector('.sidebar')
 export const taskContainer = document.querySelector('.tasks')
-export const defaultProjects = document.querySelector('.default-projects-list')
+export const projects = document.querySelector('.default-projects-list')
+const userProjectContainer = document.querySelector('.user-projects')
 
 export const addProjectButton = document.querySelector('.add-project-button')
 export const addTaskButton = document.querySelector('.add-task-button')
@@ -10,23 +13,55 @@ export const addTaskButton = document.querySelector('.add-task-button')
 export const modalOverlay = document.querySelector('#modal-overlay')
 export const submitButton = document.querySelector('.submit-task-button')
 
+export const confirmProjectButton = document.querySelector('.confirm-project') 
+export const projectNameInput = document.querySelector('.project-name-input-field')
+
+
+let projectNameDiv = document.body.querySelector('.project-name-input')
 
 
 
+export let projectList = [projects.children[0],projects.children[1],projects.children[2]]
 
-    for(let i = 0; i < defaultProjects.children.length;i++){
-        defaultProjects.children[i].addEventListener('click', ()=>{
 
-            for(let j = 0; j < defaultProjects.children.length;j++){
-                defaultProjects.children[j].classList.remove('highlighted');
-            }
-  
-            defaultProjects.children[i].classList.add('highlighted');
-    });
-    }
+export function drawNewProjectContainer(name){
+
+    let newProjectContainer = document.createElement('button')
+    newProjectContainer.innerText = name
+    userProjectContainer.appendChild(newProjectContainer)
+    projectNameDiv.style.display = 'none'
+    projectList.push(newProjectContainer)
+//     newProjectContainer.addEventListener('click',()=>{
     
+//         for(let j = 0; j < projects.children.length;j++){
+//             projectList[j].classList.remove('highlighted');
+//         }
+  
+//         newProjectContainer.classList.add('highlighted');
+
+// })
+
+for(let i = 0; i < projectList.length;i++){
+    projectList[i].addEventListener('click', ()=>{
+ 
+
+        for(let j = 0; j < projectList.length;j++){
+            projectList[j].classList.remove('highlighted');
+        }
+  
+        projectList[i].classList.add('highlighted');
+
+});
+}
+}    
   
 
+
+export function getProjectName(){
+
+    projectNameDiv.style.display = 'flex'
+
+}
 
 export function drawTask(toDo){
 
@@ -104,6 +139,7 @@ function drawProjects(){
 addTaskButton.addEventListener('click', ()=>{
 
     modalOverlay.style.display = 'flex'
+
 
 
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../src/style.css';
-import { drawTask,addProjectButton,addTaskButton,modalOverlay,submitButton,gatherTaskInfo} from './DOMstuff';
+import { drawTask,projects,projectList,confirmProjectButton,projectNameInput,addProjectButton,drawNewProjectContainer,getProjectName,addTaskButton,modalOverlay,submitButton,gatherTaskInfo} from './DOMstuff';
 import {createToDo,inbox} from './logic'
 
 
@@ -16,6 +16,41 @@ submitButton.addEventListener('click',()=>{
     modalOverlay.style.display = 'none'
 
 })
+
+
+export function addProjectEventListeners(){
+
+    for(let i = 0; i < projectList.length;i++){
+        projectList[i].addEventListener('click', ()=>{
+     
+    
+            for(let j = 0; j < projects.children.length;j++){
+                projectList[j].classList.remove('highlighted');
+            }
+      
+            projectList[i].classList.add('highlighted');
+    
+    });
+    }
+}
+
+addProjectEventListeners()
+
+addProjectButton.addEventListener('click',()=>{
+
+    getProjectName()    
+
+})
+
+confirmProjectButton.addEventListener('click', ()=>{
+    let newProjectName = projectNameInput.value
+    drawNewProjectContainer(newProjectName)
+})
+
+
+
+
+
 
 
 
